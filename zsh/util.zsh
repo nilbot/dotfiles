88729d2ea2d809fed181718bf77affbbc2fb51f1
Nick Nisi <nick@nisi.org>
2015-10-27 22:13:32 +0000

    fixed error in util function

--- zsh/util.zsh
+++ zsh/util.zsh
@@ -3,6 +3,6 @@ function socks() export http_proxy=socks5://127.0.0.1:1080 https_proxy=socks5://
 function usocks() unset http_proxy https_proxy
 
 # calculate space for current folder
-function space() 'du -ch | grep total'
+function space() du -ch | grep total
 
 
