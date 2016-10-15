ddaee251945a405adf8b703760e2d2de606f67da
Ersi Ni <ersi.ni@nilbot.net>
2016-10-15 14:14:52 +0100

    no confirmation interaction for pacman

--- install-dep.sh
+++ install-dep.sh
@@ -13,15 +13,15 @@ if [ "$(uname)" == "Darwin" ];then
     done
 elif [ "$(uname -r)" == *"ARCH" ]; then
     echo "installing prerequisite softwares using pacman and AUR..."
-    sudo pacman -S base-devel
+    sudo pacman -S --noconfirm base-devel
     sudo echo <<EOF >> /etc/pacman.conf
 [archlinuxfr]
 SigLevel = Never
 Server = https://repo.archlinux.fr/$arch
 EOF
-    sudo pacman -Syy && sudo pacman -S yaourt
-    sudo pacman -S gnupg python ruby git go
-    yaourt -S the_platinum_searcher
+    sudo pacman -Syy && sudo pacman -S --noconfirm yaourt
+    sudo pacman -S --noconfirm gnupg python ruby git go
+    yaourt -S --noconfirm the_platinum_searcher
 else
     echo "Your platform is not yet supported. Install the softwares manually please."
     exit 127
