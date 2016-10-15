96f6dc35d3493be9045f108ca1e0e47a3620a595
Ersi Ni <ersi.ni@nilbot.net>
2016-10-15 13:57:33 +0100

    added install-dep.sh, not tested

---
+++ install-dep.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+if [ "$(uname)" == "Darwin" ];then
+    echo "installing prerequisite softwares using brew..."
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    echo "installing packages..."
+    source brew/brew.tap
+    for pkg in $(cat brew/brew.list); do
+        brew install $pkg
+    done
+    for pkg in $(cat brew/brew.cask.list); do
+        brew cask install $pkg
+    done
+elif [ "$(uname -r)" == *"ARCH" ]; then
+    echo "installing prerequisite softwares using pacman and AUR..."
+    sudo pacman -S base-devel
+    sudo echo <<EOF >> /etc/pacman.conf
+[archlinuxfr]
+SigLevel = Never
+Server = https://repo.archlinux.fr/$arch
+EOF
+    sudo pacman -Syy && sudo pacman -S yaourt
+    sudo pacman -S gnupg python ruby git go
+    yaourt -S the_platinum_searcher
+else
+    echo "Your platform is not yet supported. Install the softwares manually please."
+    exit 127
+fi
+exit 0
