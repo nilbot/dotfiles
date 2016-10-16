3a74ce17a82f0b721c6460ba0d5d75ae420b0ddc
Ersi Ni <ersi.ni@nilbot.net>
2016-10-16 22:21:00 +0100

    add shellcheck to arch

--- super-install-dep.sh
+++ super-install-dep.sh
@@ -20,7 +20,7 @@ SigLevel = Never
 Server = https://repo.archlinux.fr/$arch
 EOF
     pacman -Syy && pacman -S --noconfirm yaourt
-    pacman -S --noconfirm gnupg python ruby git go
+    pacman -S --noconfirm gnupg python ruby git go shellcheck
 else
     echo "Your platform is not yet supported. Install the softwares manually please."
     exit 127
