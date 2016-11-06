2547dc8a365b7f5f50ed835a5e865ed7a077b23a
Ersi Ni <ersi.ni@nilbot.net>
2016-11-06 17:00:52 +0000

    add ubuntu support

--- super-install-dep.sh
+++ super-install-dep.sh
@@ -1,30 +1,23 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 if [[ "$(uname)" == "Darwin" ]];then
-    echo "installing prerequisite softwares using brew..."
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    echo "installing packages..."
-    source brew/brew.tap
-    while read -r pkg
-    do
-        brew install "$pkg"
-    done < brew/brew.list
-    while read -r pkg
-    do
-        brew cask install "$pkg"
-    done < brew/brew.cask.list
+		xcode-select --install
 elif [[ "$(uname -r)" == *"ARCH" ]]; then
-    echo "installing prerequisite softwares using pacman and AUR..."
-    pacman -S --noconfirm base-devel
-    cat <<'EOF' >> /etc/pacman.conf
+		echo "installing prerequisite softwares using pacman and AUR..."
+		pacman -S --noconfirm base-devel
+		cat <<'EOF' >> /etc/pacman.conf
 [archlinuxfr]
 SigLevel = Never
 Server = https://repo.archlinux.fr/$arch
 EOF
-    pacman -Syy && pacman -S --noconfirm yaourt
-    pacman -S --noconfirm gnupg python ruby git go shellcheck zsh
+		pacman -Syy && pacman -S --noconfirm yaourt
+		pacman -S --noconfirm gnupg python ruby git go shellcheck zsh
+elif [[ "$(uname -v)" =~"Ubuntu" ]];then
+		apt-get update && apt-get upgrade -y
+		apt-get install -y build-essential
+		apt-get install -y ruby zsh
 else
-    echo "Your platform is not yet supported. Install the softwares manually please."
-    exit 127
+		echo "Your platform is not yet supported. Install the softwares manually please."
+		exit 127
 fi
 exit 0
--- user-install-dep.sh
+++ user-install-dep.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
 if [[ "$(uname -r)" == *"ARCH" ]]; then
-	yaourt -S --noconfirm the_platinum_searcher
+		yaourt -S --noconfirm the_platinum_searcher
+elif [[ "$(uname -v)" =~ "Ubuntu" ]];then
+		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+elif [[ "$(uname)" == "Darwin" ]]; then
+		echo "installing prerequisite softwares using brew..."
+		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+		echo "installing packages..."
+		source brew/brew.tap
+		while read -r pkg
+		do
+				brew install "$pkg"
+		done < brew/brew.list
+		while read -r pkg
+		do
+				brew cask install "$pkg"
+		done < brew/brew.cask.list
 fi
