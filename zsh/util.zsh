9fb3f3d096b25975e63d5f48a5c11624dec6e4ed
Ersi Ni <ersi.ni@nilbot.net>
2017-07-26 09:00:03 +0800

    Updated settings for git,zsh updated proxy settings for zsh

--- zsh/util.zsh
+++ zsh/util.zsh
@@ -1,5 +1,8 @@
 # socks proxy
-function socks() export http_proxy=socks5://127.0.0.1:1080 https_proxy=socks5://127.0.0.1:1080
+function socks() {
+    local_socks5=http://127.0.0.1:58173
+    export http_proxy=$local_socks5 https_proxy=$local_socks5
+}
 function usocks() unset http_proxy https_proxy
 
 # calculate space for current folder
@@ -13,3 +16,6 @@ if [[ -e /usr/local/bin/gfind  ]]; then
 	alias find='gfind'
 fi
 
+if [[ $(hostname) == "airbot.modouwifi.com" ]]; then
+    socks
+fi
