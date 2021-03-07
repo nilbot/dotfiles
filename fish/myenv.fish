435c7c1f728ad1004abca6820b3ceb729154961e
Ersi Ni <ersi.ni@nilbot.net>
2021-03-07 22:56:30 +0000

    miniconda fish

--- fish/myenv.fish
+++ fish/myenv.fish
@@ -49,9 +49,10 @@ if test -d /usr/local/share/dotnet
 end
 
 # miniconda (eval bugged)
-if test -d $HOME/miniconda3
+if test -d $HOME/miniconda3/bin
     # eval $HOME/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-    fish_add_path $HOME/miniconda3
+    fish_add_path $HOME/miniconda3/bin
+    # source $HOME/miniconda3/etc/fish/conf.d/conda.fish
 end
 
 # flutter
