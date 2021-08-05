e1c3e5e2ea7db8a3138bee2c296fbef59f0cb9ce
Ersi Ni <ersi.ni@nilbot.net>
2021-08-05 11:47:41 +0800

    fix bit completion

--- fish/completions/bit.fish
+++ fish/completions/bit.fish
@@ -3,7 +3,7 @@ function __complete_bit
     set -lx COMP_LINE (commandline -cp)
     test -z (commandline -ct)
     and set COMP_LINE "$COMP_LINE "
-    /usr/local/bin/bit
+    /opt/homebrew/bin/bit
 end
 complete -f -c bit -a "(__complete_bit)"
 
