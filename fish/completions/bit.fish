6aad0cc64cb2a68523a7a8c6c55430de827252b2
Ersi Ni <ersi.ni@nilbot.net>
2021-08-23 23:29:20 +0800

    fish's bit completion fix don't hardcode path to bit

--- fish/completions/bit.fish
+++ fish/completions/bit.fish
@@ -1,9 +1,9 @@
-
+set BIT_BIN (which bit)
 function __complete_bit
     set -lx COMP_LINE (commandline -cp)
     test -z (commandline -ct)
     and set COMP_LINE "$COMP_LINE "
-    /opt/homebrew/bin/bit
+    $BIT_BIN
 end
 complete -f -c bit -a "(__complete_bit)"
 
