00670bcdc47ab13484087d40cc52d12cad5822a1
Ersi Ni <ersi.ni@nilbot.net>
2021-11-23 22:06:31 +0000

    moved google cloud sdk into sdk

--- fish/mypre.fish
+++ fish/mypre.fish
@@ -24,12 +24,12 @@ if test -d $HOME/.cargo/bin
 end
 
 # gcloud
-if test -d $HOME/devel/google-cloud-sdk
-    # source $HOME/devel/google-cloud-sdk/path.fish.inc
+if test -d $HOME/sdk/google-cloud-sdk
+    # source $HOME/sdk/google-cloud-sdk/path.fish.inc
     # NOT USING official google cloud sdk fish path script
     # Why? The author claims they don't really understand fish.
     # and apparently sourcing this file doesn't work across sessions.
-    fish_add_path $HOME/devel/google-cloud-sdk/bin
+    fish_add_path $HOME/sdk/google-cloud-sdk/bin
 end
 if test (uname) = "Darwin"
     set -x CLOUDSDK_PYTHON /usr/bin/python
