516d137439299f5bfec1aa550eace4d1be8dd8d6
Ersi Ni <ersi.ni@nilbot.net>
2023-03-22 18:46:56 +0000

    fix dotenv read line fail on empty lines

--- fish/mypre.fish
+++ fish/mypre.fish
@@ -138,8 +138,8 @@ end
 function readenv --on-variable PWD
     if test -r .env
         cat .env | while read -l line
-            # split on the first =, and set the variable, but only if the starting character is not #
-            if test (string sub -s 1 -l 1 $line) != "#"
+            # split on the first =, and set the variable, but only if the line is not empty and the starting character is not #
+            if test -n $line; and test (string sub -s 1 -l 1 $line) != "#"
                 set -l kv (string split -m 1 = -- $line)
                 set -gx $kv # this will set the variable named by $kv[1] to the rest of $kv
             end
