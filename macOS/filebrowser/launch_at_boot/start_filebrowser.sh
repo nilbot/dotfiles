1cbe8d8e9aca1b0affa472582389894af8e07a69
Ersi Ni <ersi.ni@nilbot.net>
2025-05-09 19:47:13 +0100

    update filebrowser listen ip address to local LAN address (in home router)

--- macOS/filebrowser/launch_at_boot/start_filebrowser.sh
+++ macOS/filebrowser/launch_at_boot/start_filebrowser.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -e
-cd $HOME && /opt/homebrew/bin/filebrowser -r $HOME/share/inbox/ -a 127.0.0.1 -p 23333
+cd $HOME && /opt/homebrew/bin/filebrowser -r $HOME/share/inbox/ -a $(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | grep -Fv 100. | awk '{print $2}') -p 23333
