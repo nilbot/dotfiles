345b2b96962a4319e64e8d12b78c2a0ccdea3e82
Ersi Ni <ersi.ni@nilbot.net>
2026-09-24 09:24:38 +0100

    fix: removed bend because it SIGKILL on my box, too unstable

--- fish/mypre.fish
+++ fish/mypre.fish
@@ -351,7 +351,3 @@ if test -d $HOME/.cache/lm-studio
     fish_add_path $LMSTUDIO_INSTALL/bin
 end
 
-# bend (higher order company's experimental language)
-if test -e $HOME/.bend/bin/bend
-    fish_add_path $HOME/.bend/bin
-end
